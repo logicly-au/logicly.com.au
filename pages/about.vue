4fd33e6e2627900b25487fabb252d95ab938a30c
Huw M <huwm@strategicdata.com.au>
2020-11-12 16:42:45 +1100

    November 12, 2020 4:42 PM

--- pages/about.vue
+++ pages/about.vue
@@ -6,7 +6,7 @@
     <div class="w-full h-64 bg-logiclytheme2 about-header" style="background-size: cover;" :style="{ backgroundImage: `url(${backgroundUrl})` }">
       <div class="container mx-auto">
         <div class="grid h-64 grid-rows-3 grid-cols-15">
-          <div class="col-span-1 col-start-2 row-start-2">
+          <div class="col-span-11 col-start-2 row-start-2">
 
 
             <h1>Our Approach</h1>
