25595b739155c28f68ca5d55c8d84482735238ea
Huw M <huwm@strategicdata.com.au>
2020-11-16 10:36:58 +1100

    November 16, 2020 10:36 AM

--- pages/ourculture.vue
+++ pages/ourculture.vue
@@ -1,1 +1,33 @@
-Our Culture
+<template>
+  <div>
+  
+    <!-- Sec 1 -->
+
+    <div class="w-full h-64 bg-logiclytheme2 about-header" style="background-size: cover;" :style="{ backgroundImage: `url(${backgroundUrl})` }">
+      <div class="container mx-auto">
+        <div class="grid h-64 grid-rows-3 grid-cols-15">
+          <div class="col-span-11 col-start-2 row-start-2">
+
+
+
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- End Sec 1 -->
+
+
+  </div>
+</template>
+
+<script>
+
+export default {}
+
+</script>
+
+<style>
+
+</style>
