271ea52ed709ec477da698fff1b041112320a2a6
Huw M <huwm@strategicdata.com.au>
2020-12-07 14:23:14 +1100

    December 7, 2020 2:23 PM

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,6 @@
     <div class="w-full h-64 bg-logiclytheme2">
       <div class="container mx-auto">
         <div class="grid h-64 grid-cols-2 grid-rows-2">
-            <div class="cic-header"  style="background-size: cover;" :style="{ backgroundImage: `url(${backgroundUrl})` }></div>
             <div class="col-span-1 col-start-2 row-start-2">
             <span class="text-xl text-white">We help you meet your complex information challenges</span>
           </div>
