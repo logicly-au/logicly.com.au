9cf3ddf9b24b173821ff5f7fcec3c8bac79b67db
Huw M <huwm@strategicdata.com.au>
2021-02-11 17:21:45 +1100

    Added footer styling

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="logicly-footer">
+    <div class="logicly-footer bg-logiclylightgrey">
         <div class="container mx-auto">
-            <footer>
+            <footer class="py-16">
             <!-- TODO Add links here -->
-                <div class="grid grid-cols-15">
-                <div class="col-span-3 text-sm text-left">
-                    <h2 class="text-lg"><logo /></h2>
+                <div class="grid row-end-1 grid-cols-15">
+                <div class="col-span-4 text-sm text-left">
+                    <!-- TODO Find a tailwindier way of doing this -->
+                    <h2 class="text-lg"><logo style="margin-top:-1rem;margin-left:-1rem" /></h2>
                     <a class="block">+61 3 9340 9000</a>
                     <a class="block">info@strategicdata.com.au</a>
                     <span class="block">2nd floor, 51 – 55 Johnston Street,</span>
                     <span class="block">Fitzroy,Victoria 3065, Australia</span>
                     <span class="block">PO Box 1166, Carlton, Victoria 3053, Australia</span>
-                    <span>Logicly © 2020</span><a>Privacy Policy</a>
                 </div>
                 <div class="grid grid-cols-3 col-span-5 col-start-10 text-sm">
                     <div class="col-span-1">
@@ -39,11 +39,22 @@
                         <a class="block" href="">Our security</a>
                         <a class="block" href="">Our culture</a>
                     </div>
-                    <img src="~/assets/images/Facebook.svg"></img>
-                    <img src="~/assets/images/Linkedin.svg"></img>
-                    <img src="~/assets/images/Twitter.svg"></img>
                 </div>
             </div>
+
+                <!-- TODO Find tailwind solution -->
+                <div class="grid col-span-15 grid-cols-15" style="align-items: end">
+                    <div class="col-span-5 text-sm">
+                        <span class="items-end col-span-4 row-start-1 pb-2">
+                        <span>Logicly © 2020</span> <a>Privacy Policy</a></span>
+                    </div>
+                    <div class="grid h-16 grid-cols-3 col-span-2 col-start-13 pb-1">
+                        <div class="col-span-1"><img src="~/assets/images/Facebook.svg" class="h-12"></img></div>
+                        <div class="col-span-1"><img src="~/assets/images/Linkedin.svg" class="h-12"></img></div>
+                        <div class="col-span-1"><img src="~/assets/images/Twitter.svg" class="h-12"></img></div>
+                    </div>
+                </div>
+                    
             </footer>
         </div>
     </div>
