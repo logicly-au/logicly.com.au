6438469d79d1bfae2df02a436046242076e2c3fb
Emmie Berg <emmieb@quietroom-imac2.wkst.sdlocal.net>
2021-03-03 16:00:39 +1100

    Changing grid section on Our Approach page to GridText component

--- pages/ourapproach.vue
+++ pages/ourapproach.vue
@@ -13,91 +13,27 @@
     subheading="We help you navigate through your complex information problem and  explore ideas and possibilities. We look at the larger context of your situation and recommend different approaches to research, design, develop, implement and maintain your digital system."
     />
 
-    <div class="w-full bg-white">
-      <div class="container mx-auto">
-        <div class="grid h-full grid-cols-5">
-          <div class="self-center col-span-3 col-start-2 pt-24 lg:pt-32">
-            <h2 class="text-3xl lg:text-2xl font-semibold text-center">We consider each project in the same way</h2>
-          </div>
-        </div>
-        <div class="grid grid-cols-1 lg:grid-cols-8 my-12">
-          <div class="grid grid-cols-1 lg:col-span-6 lg:col-start-2">
-            <div class="grid grid-cols-1 lg:grid-cols-3 gap-5 lg:pb-20">
-              <div class="col-span-1 bg-white px-12 lg:px-4 pb-12 lg:pb-0">
-                <p class="text-2xl lg:text-xl font-semibold pb-2">
-                  Discovery
-                </p>
-                <p class="text-md font-light pb-1">
-                  We always take time at the start to deeply understand our client’s information problem but also pinpoint what they want to achieve and why.
-                </p>
-                <p class="text-md font-light">
-                  This holistic view helps us determine the right approach and the different pathways to meet their needs for now and the future.
-                </p>
-              </div>
-
-              <div class="col-span-1 bg-white px-12 lg:px-4 pb-12 lg:pb-0">
-                <p class="text-2xl lg:text-xl font-semibold pb-2">
-                  Clear explanations
-                </p>
-                <p class="text-md font-light pb-1">
-                  We break down complex IT concepts into simple, straight forward explanations and stay clear from technical terms.
-                </p>
-                <p class="text-md font-light">
-                  We want our clients to clearly understand our approach and the benefits so they can make a fully informed decision.
-                </p>
-              </div>
-
-              <div class="col-span-1 bg-white px-12 lg:px-4 pb-12 lg:pb-0">
-                <p class="text-2xl lg:text-xl font-semibold pb-2">
-                  Creating efficiencies
-                </p>
-                <p class="text-md font-light pb-1">
-                  We review how clients are working with their information and design and build digital systems that are faster, have checks and balances, and reduce human error. We use automated processes to cut costs in time and mistakes.
-                </p>
-              </div>
-            </div>
-
-            <div class="grid grid-cols-1 lg:grid-cols-3 gap-5 lg:pb-20">
-              <div class="col-span-1 bg-white px-12 lg:px-4 pb-12 lg:pb-0">
-                <p class="text-2xl lg:text-xl font-semibold pb-2">
-                  Sharing knowledge
-                </p>
-                <p class="text-md font-light pb-1">
-                  We achieve the best outcomes when we’re part of your team and understand the needs of everyone invloved.
-                </p>
-                <p class="text-md font-light">
-                  We enjoy sharing our expertise and supporting our clients to develop their own capabilities.
-                </p>
-              </div>
-
-              <div class="col-span-1 bg-white px-12 lg:px-4 pb-12 lg:pb-0">
-                <p class="text-2xl lg:text-xl font-semibold pb-2">
-                  No short cuts
-                </p>
-                <p class="text-md font-light pb-1">
-                  We won’t build a system without considering the context, but we also understand that there are times when you need to take a short cut.
-                </p>
-                <p class="text-md font-light">
-                  Our experience means we know when to raise issues with clients and how to help you manage the risks.
-                </p>
-              </div>
-
-              <div class="col-span-1 bg-white px-12 lg:px-4 pb-12 lg:pb-0">
-                <p class="text-2xl lg:text-xl font-semibold pb-2">
-                  Intuitive, user friendly design
-                </p>
-                <p class="text-md font-light pb-1">
-                  We ensure the user is at the centre of any decisions when designing, developing and editing a digital system.
-                </p>
-                <p class="text-md font-light">
-                  Our team of UI, UX designers and staff with backgrounds in psychology, all work to ensure there are no barriers to access information for all users.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <GridText
+    heading="We consider each project in the same way"
+    subheading1="Discovery"
+    text1a="We always take time at the start to deeply understand our client’s information problem but also pinpoint what they want to achieve and why."
+    text1b="This holistic view helps us determine the right approach and the different pathways to meet their needs for now and the future."
+    subheading2="Clear explanations"
+    text2a="We break down complex IT concepts into simple, straight forward explanations and stay clear from technical terms."
+    text2b="We want our clients to clearly understand our approach and the benefits so they can make a fully informed decision."
+    subheading3="Creating efficiencies"
+    text3a="We review how clients are working with their information and design and build digital systems that are faster, have checks and balances, and reduce human error. We use automated processes to cut costs in time and mistakes."
+    text3b=""
+    subheading4="Sharing knowledge"
+    text4a="We achieve the best outcomes when we’re part of your team and understand the needs of everyone involved."
+    text4b="We enjoy sharing our expertise and supporting our clients to develop their own capabilities."
+    subheading5="No short cuts"
+    text5a="We won’t build a system without considering the context, but we also understand that there are times when you need to take a short cut."
+    text5b="Our experience means we know when to raise issues with clients and how to help you manage the risks."
+    subheading6="Intuitive, user friendly design"
+    text6a="We ensure the user is at the centre of any decisions when designing, developing and editing a digital system."
+    text6b="Our team of UI, UX designers and staff with backgrounds in psychology, all work to ensure there are no barriers to access information for all users."
+    />
 
     <Hero
     subheading="It’s like a puzzle, gathering and piecing together all the information with clarity"
