8ce383cf8416215f60554dcc597e0bc7ec7927fb
ash <ash@strategicdata.com.au>
2021-03-18 15:18:19 +1100

    Fix vue-tabs

--- plugins/vue-tabs.js
+++ plugins/vue-tabs.js
@@ -1,4 +1,5 @@
 //global registration
+import Vue from 'vue'
 import VueTabs from 'vue-nav-tabs'
 import 'vue-nav-tabs/themes/vue-tabs.css'
-// Vue.use(VueTabs) //Commenting out so the page won't break
+Vue.use(VueTabs)
