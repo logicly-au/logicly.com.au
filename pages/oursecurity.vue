ebbdf1944ce4c57a9e7b12e6b104d98a3dec4b89
Emmie Berg <emmieb@strategicdata.com.au>
2021-03-29 11:05:14 +1100

    Making 'A continuous, systematic approach' section on Security page responsive

--- pages/oursecurity.vue
+++ pages/oursecurity.vue
@@ -15,71 +15,71 @@
     />
 
     <page-section>
-      <div class="grid grid-cols-9 gap-4 text-logiclytextgrey">
-        <div class="col-span-9">
-          <h1 class="mb-4 text-lg font-bold">A continuous, systematic approach to risk analysis and security </h1>
-          <p class="mb-4">To ensure we’re at the cutting edge of security best-practices, and align with their needs, we keep a rigorous routine of continuous review and investment in our methods. Our data solutions are created to respond to each client’s unique information challenge, including their risk requirements.</p>
-          <h1 class="text-lg font-bold">Security measures we build into our projects include:</h1>
+      <div class="grid grid-cols-8 gap-x-4 text-logiclytextgrey px-0 lg:px-4 xl:px-6">
+        <div class="col-span-8 text-center">
+          <h1 class="text-2xl font-extrabold pb-4">A continuous, systematic approach to risk analysis and security </h1>
+          <p class="mb-10 text-lg">To ensure we’re at the cutting edge of security best-practices, and align with their needs, we keep a rigorous routine of continuous review and investment in our methods. Our data solutions are created to respond to each client’s unique information challenge, including their risk requirements.</p>
+          <h1 class="text-xl font-extrabold mb-6">Security measures we build into our projects include:</h1>
         </div>
-        <div class="col-span-4">
+        <div class="col-span-8 lg:col-span-4 col-start-1">
           <ul class="text-sm list-disc">
             <li>24/7 security monitoring</li>
             <li>Vulnerability scanning and penetration testing</li>
             <li>OWASP Top 10 compliancy testing</li>
-            <li>Australian Signals Directorate’s (ASD)	Essential Eight Strategy assessment</li>
-            <li>ASD Information Security Registered	Assessors Program (IRAP) assessment</li>
+            <li>Australian Signals Directorate’s (ASD) Essential Eight Strategy assessment</li>
+            <li>ASD Information Security Registered	Assessors Program (IRAP) assessment</li>
           </ul>
         </div>
-        <div class="col-span-4 col-start-5">
+        <div class="col-span-8 lg:col-span-4 lg:col-start-5">
           <ul class="text-sm list-disc">
-            <li>Development of project-specific security	documentation such as: Security Risk	Management Plans, Incident Response 	Plans, Statements of Applicability, and 	Compliance Audits</li>
-            <li>Consultation with external subject-matter	specialists as needed</li>
+            <li>Development of project-specific security documentation such as: Security Risk Management Plans, Incident Response Plans, Statements of Applicability, and Compliance Audits</li>
+            <li>Consultation with external subject-matter	specialists as needed</li>
           </ul>
         </div>
       </div>
     </page-section>
 
-  <Hero
-    subheading="We invest significant resources to ensure our systems are secure and robust."
-    background-url="/We-invest-significant-resources-to-ensure-our-systems-are-secure-and-robust.png"
-    right
-  />
+    <Hero
+      subheading="We invest significant resources to ensure our systems are secure and robust."
+      background-url="/We-invest-significant-resources-to-ensure-our-systems-are-secure-and-robust.png"
+      right
+    />
 
-  <!-- TODO add links here also fix formatting -->
-  <page-section>
-    <div class="grid grid-cols-3 gap-x-2 text-logiclytextgrey px-0 xl:px-10">
-      <div class="col-span-3 pb-8 lg:pb-4">
-        <h1 class="text-2xl font-extrabold text-center pb-4 lg:pb-8 pt-8">Protecting critical information for established organisations</h1>
-        <p class="text-lg text-center">Our comprehensive approach to security has enabled us to foster successful, long-term partnerships with leading organisations such as the Department of Health, National Australia Bank and the University of Melbourne over the course of many years, and many projects. </p>
-      </div>
-      <div class="col-span-3 lg:col-span-1 text-center flex pb-6 lg:pb-0">
-        <div class="m-auto">
-          <div class="h-24 flex items-center justify-center">
-            <img src="/Australian-Government-Federal-Department-of-Health.png" class="h-28" />
+    <!-- TODO add links here also fix formatting -->
+    <page-section>
+      <div class="grid grid-cols-3 gap-x-2 text-logiclytextgrey px-0 xl:px-10">
+        <div class="col-span-3 pb-8 lg:pb-4">
+          <h1 class="text-2xl font-extrabold text-center pb-4 lg:pb-8 pt-8">Protecting critical information for established organisations</h1>
+          <p class="text-lg text-center">Our comprehensive approach to security has enabled us to foster successful, long-term partnerships with leading organisations such as the Department of Health, National Australia Bank and the University of Melbourne over the course of many years, and many projects. </p>
+        </div>
+        <div class="col-span-3 lg:col-span-1 text-center flex pb-6 lg:pb-0">
+          <div class="m-auto">
+            <div class="h-24 flex items-center justify-center">
+              <img src="/Australian-Government-Federal-Department-of-Health.png" class="h-28" />
+            </div>
+            <NuxtLink to="/" class="text-logiclyorange underline">View Project</NuxtLink>
           </div>
-          <NuxtLink to="/" class="text-logiclyorange underline">View Project</NuxtLink>
+        </div>
+        <div class="col-span-3 lg:col-span-1 text-center flex pb-6 lg:pb-0">
+          <div class="m-auto">
+            <div class="h-24 flex items-center justify-center">
+              <img src="/The-University-of-Melbourne.png" class="h-28" />
+            </div>
+            <NuxtLink to="/" class="text-logiclyorange underline">View Project</NuxtLink>
+          </div>
+        </div>
+        <div class="col-span-3 lg:col-span-1 text-center flex">
+          <div class="m-auto">
+            <div class="h-24 flex items-center justify-center">
+              <img src="/National-Australia-Bank.png" class="h-20" />
+            </div>
+            <NuxtLink to="/" class="text-logiclyorange underline">View Project</NuxtLink>
+          </div>
         </div>
       </div>
-      <div class="col-span-3 lg:col-span-1 text-center flex pb-6 lg:pb-0">
-        <div class="m-auto">
-          <div class="h-24 flex items-center justify-center">
-            <img src="/The-University-of-Melbourne.png" class="h-28" />
-          </div>
-          <NuxtLink to="/" class="text-logiclyorange underline">View Project</NuxtLink>
-        </div>
-      </div>
-      <div class="col-span-3 lg:col-span-1 text-center flex">
-        <div class="m-auto">
-          <div class="h-24 flex items-center justify-center">
-            <img src="/National-Australia-Bank.png" class="h-20" />
-          </div>
-          <NuxtLink to="/" class="text-logiclyorange underline">View Project</NuxtLink>
-        </div>
-      </div>
-    </div>
-  </page-section>
+    </page-section>
 
-</div>
+  </div>
 </template>
 
 <script>
