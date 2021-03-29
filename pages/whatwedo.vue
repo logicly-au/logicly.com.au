63b6d354c017bb9b683d722b965b776ab4148511
Emmie Berg <emmieb@strategicdata.com.au>
2021-03-29 16:58:32 +1100

    Updating the ‘Our collaborative journey' and 'Every information challenge is different' sections on 'How we work' page and making them responsive

--- pages/whatwedo.vue
+++ pages/whatwedo.vue
@@ -1,121 +1,129 @@
 <template>
   <div>
-  
+
     <Hero
-    heading="WHAT WE DO"
-    subheading="From discovery to development, delivery and beyond"
-    background-url="/Logicly_What-we-do-header-From-discovery-to-development-delivery-and-beyond.jpg"
-    overlay
+      heading="WHAT WE DO"
+      subheading="From discovery to development, delivery and beyond"
+      background-url="/Logicly_What-we-do-header-From-discovery-to-development-delivery-and-beyond.jpg"
+      overlay
     />
 
     <Herotext
-    iconurl="We-start-with-the-right-questions-to-gain-insightful-answers.svg"
-    heading="We start with the right questions to gain insightful answers"
-    subheading="Our services are comprehensive and thorough; from initial consulting and design through to build and delivery. We continuously review results, outcomes and user experience to ensure the needs of all stakeholders are met."
-    altrow
+      iconurl="We-start-with-the-right-questions-to-gain-insightful-answers.svg"
+      heading="We start with the right questions to gain insightful answers"
+      subheading="Our services are comprehensive and thorough; from initial consulting and design through to build and delivery. We continuously review results, outcomes and user experience to ensure the needs of all stakeholders are met."
+      altrow
     />
 
     <page-section>
-      <div class="grid grid-cols-11 col-span-11 col-start-3 mt-4">
-        <div class="col-span-11">
-          <h2 class="font-bold text-center">Scope of work and services</h2>
+      <div class="grid grid-cols-12 gap-6 lg:gap-8 mt-4 -mb-10 lg:mb-6 mx-0 md:mx-6 lg:mx-0">
+        <div class="col-span-12">
+          <h2 class="text-2xl font-extrabold text-center text-logiclytextgrey pb-4 lg:pb-8 pt-8">Scope of work and services</h2>
         </div>
-          <div class="col-span-3 border-t-2 border-logiclyorange">
-          <h2 class="font-bold">Research</h2>
-          </div>
-          <div class="col-span-4 border-t-2 border-logiclyorange">
-          <p>We work closely with our clients to understand the nuances and  complexities they face, so that we can make sure we’re providing the right solution for their needs.</p>
-          </div>
-          <div class="col-span-4 border-t-2 border-logiclyorange">
-          <h2 class="font-bold">This may include:</h2>
-          <ul class="ml-4 list-disc">
-            <li>Workshop/initial consultation </li>
+        <div class="col-span-12 border-t-2 border-logiclytheme1">
+        </div>
+        <div class="col-span-12 lg:col-span-3 -mb-3">
+          <h2 class="text-xl font-extrabold">Research</h2>
+        </div>
+        <div class="col-span-12 lg:col-span-5 font-light lg:pr-6 -mb-2">
+          <p>We work closely with our clients to understand the nuances and complexities they face, so that we can make sure we’re providing the right solution for their needs.</p>
+        </div>
+        <div class="col-span-12 lg:col-span-4">
+          <h2 class="font-extrabold">This may include:</h2>
+          <ul class="ml-4 font-light text-sm list-disc">
+            <li>Workshop/initial consultation</li>
             <li>Project Scope</li>
             <li>Strategic advice</li>
             <li>Planning</li>
           </ul>
-          </div>
-          <div class="col-span-3 border-t-2 border-logiclyorange">
-          <h2 class="font-bold">Design</h2>
-          </div>
-          <div class="col-span-4 border-t-2 border-logiclyorange">
+        </div>
+
+        <div class="col-span-12 border-t-2 border-logiclytheme1">
+        </div>
+        <div class="col-span-12 lg:col-span-3 -mb-3">
+          <h2 class="text-xl font-extrabold">Design</h2>
+        </div>
+        <div class="col-span-12 lg:col-span-5 font-light lg:pr-6 -mb-2">
           <p>After thoughtfully considering our clients’ information challenges, we design user-friendly, considered digital solutions that are tailored to support their needs.</p>
-          </div>
-          <div class="col-span-4 border-t-2 border-logiclyorange">
-          <h2 class="font-bold">We consider:</h2>
-          <ul class="ml-4 list-disc">
+        </div>
+        <div class="col-span-12 lg:col-span-4">
+          <h2 class="font-extrabold">We consider:</h2>
+          <ul class="ml-4 font-light text-sm list-disc">
             <li>Dataflow</li>
             <li>System architecture</li>
             <li>Database design</li>
-            <li>Information visualisation</li> 
+            <li>Information visualisation</li>
             <li>User experience & accessibility (UX)</li>
             <li>System security</li>
           </ul>
-          </div>
-          <div class="col-span-3 border-t-2 border-logiclyorange">
-          <h2 class="font-bold">Build</h2>
-          </div>
-          <div class="col-span-4 border-t-2 border-logiclyorange">
-            <p>We build robust digital solutions that provide our clients with an outcome that works for them. We develop systems with a focus on data integrity, secure management of all digital information and a proven track record delivering complex projects on time.</p>
-          </div>
-          <div class="col-span-4 border-t-2 border-logiclyorange">
-            <h2 class="font-bold">This may involve:</h2>
-            <ul class="ml-4 list-disc">
-              <li>Applications</li>
-              <li>API Development</li>
-              <li>MVP (minimum viable product)</li>
-              <li>Collating multiple data sources</li>
-              <li>Document management systems</li>
-              <li>Data migration & transformation</li>
-              <li>QA & testing</li>
-            </ul>
-            <h2 class="font-bold">Our build expertise extends to:</h2>
-            <ul class="ml-4 list-disc">
-              <li>Data security & privacy</li>
-              <li>Data collection & management principles</li>
-              <li>Information accuracy</li>
-              <li>Error prevention / data validation</li>
-            </ul>
-          </div>
-          <div class="col-span-3 border-t-2 border-logiclyorange">
-          <h2 class="font-bold">Maintain & Improve</h2>
-          </div>
-          <div class="col-span-4 border-t-2 border-logiclyorange">
+        </div>
+
+        <div class="col-span-12 border-t-2 border-logiclytheme2">
+        </div>
+        <div class="col-span-12 lg:col-span-3 -mb-3">
+          <h2 class="text-xl font-extrabold">Build</h2>
+        </div>
+        <div class="col-span-12 lg:col-span-5 font-light lg:pr-6 -mb-2">
+          <p>We build robust digital solutions that provide our clients with an outcome that works for them. We develop systems with a focus on data integrity, secure management of all digital information and a proven track record delivering complex projects on time.</p>
+        </div>
+        <div class="col-span-12 lg:col-span-4">
+          <h2 class="font-extrabold">This may involve:</h2>
+          <ul class="ml-4 font-light text-sm list-disc">
+            <li>Applications</li>
+            <li>API Development</li>
+            <li>MVP (minimum viable product)</li>
+            <li>Collating multiple data sources</li>
+            <li>Document management systems</li>
+            <li>Data migration & transformation</li>
+            <li>QA & testing</li>
+          </ul>
+          <h2 class="font-extrabold mt-4">Our build expertise extends to:</h2>
+          <ul class="ml-4 font-light text-sm list-disc">
+            <li>Data security & privacy</li>
+            <li>Data collection & management principles</li>
+            <li>Information accuracy</li>
+            <li>Error prevention / data validation</li>
+          </ul>
+        </div>
+
+        <div class="col-span-12 border-t-2 border-logiclytheme3">
+        </div>
+        <div class="col-span-12 lg:col-span-3 -mb-3">
+          <h2 class="text-xl font-extrabold">Maintain & Improve</h2>
+        </div>
+        <div class="col-span-12 lg:col-span-5 font-light lg:pr-6 -mb-2">
           <p>A great digital solution should be easy to use for the people who need it. We’ll be working alongside you to help smooth any bumps along the way.</p>
-          </div>
-          <div class="col-span-4 border-t-2 border-logiclyorange">
-          <h2 class="font-bold">We provide:</h2>
-          <ul class="ml-4 list-disc">
+        </div>
+        <div class="col-span-12 lg:col-span-4">
+          <h2 class="font-extrabold">We provide:</h2>
+          <ul class="ml-4 font-light text-sm list-disc">
             <li>Ongoing security expertise</li>
             <li>Hosting, storage and maintenance</li>
             <li>Database management</li>
             <li>Support help desk</li>
             <li>User training</li>
           </ul>
-          </div>
+        </div>
+        <div class="col-span-12 border-t-2 border-logiclytheme3">
+        </div>
       </div>
     </page-section>
 
     <page-section>
-
-          <div class="grid grid-cols-2 col-span-11 col-start-3 mt-4">
-            <div class="col-span-11 col-start-3">
-              <img src="~/assets/images/Every_information_challenge-is-different-and-requires-a-unique-digital-solution.jpg" />
-            </div>
-            <div class="text-center col-span-15">
-              <h1 class="text-lg font-bold text-center text-logiclytextgrey">Every information challenge is different and requires a unique digital solution</h1>
-            </div>
-            <div class="grid justify-center col-start-2 col-span-15 align-center">
-              <div class="pb-12">
-                <a class="block p-2 m-5 text-white bg-logiclyorange">View our projects</a>
-              </div>
-            </div>
+      <div class="grid grid-cols-12">
+        <div class="col-span-12 mb-12">
+          <img src="~/assets/images/Every_information_challenge-is-different-and-requires-a-unique-digital-solution.jpg" class="p-0"/>
+        </div>
+        <div class="col-span-12 lg:col-span-10 lg:col-start-2 xl:col-span-8 xl:col-start-3 text-center">
+          <h1 class="text-2xl font-extrabold text-center pb-2 text-logiclytextgrey">Every information challenge is different and requires a unique digital solution</h1>
+        </div>
+        <div class="col-span-12 grid justify-center align-center">
+          <div class="pb-12">
+            <a class="block p-3 m-5 text-white bg-logiclyorange">View our projects</a>
           </div>
-            
+        </div>
+      </div>
     </page-section>
-  </div>
-
-
 
   </div>
 </template>
