8b52ebcb951268a2c8fbf56d115895dd47163ce3
Emmie Berg <emmieb@strategicdata.com.au>
2021-03-31 16:45:21 +1100

    Updating 'Start the conversation' section on 'Contact us' page

--- pages/contactus.vue
+++ pages/contactus.vue
@@ -1,92 +1,93 @@
 <template>
   <div>
 
-  <Hero
-    heading="CONTACT US"
-    subheading="We clarify complex information challenges with digital systems that support impactful change and growth."
-    background-url="/Logicly_Contact_us_header.jpg"
-    right
-    overlay
-  />
+    <Hero
+      heading="CONTACT US"
+      subheading="We clarify complex information challenges with digital systems that support impactful change and growth."
+      background-url="/Logicly_Contact_us_header.jpg"
+      right
+      overlay
+    />
 
-    <div class="w-full bg-white">
-      <div class="container mx-auto">
-        <div class="grid h-full grid-cols-5">
-          <div class="self-center col-span-3 col-start-2 pt-12 lg:pt-20">
-            <h2 class="text-2xl font-extrabold text-center text-logiclytextgrey">Start the conversation about how we can help</h2>
+
+    <page-section>
+      <div class="grid grid-cols-5">
+        <div class="self-center col-span-5 mx-12 lg:mx-0 pt-12 lg:pt-20">
+          <h2 class="text-2xl font-extrabold text-center text-logiclytextgrey">Start the conversation about how we can help</h2>
+        </div>
+      </div>
+      <div class="grid grid-cols-3 gap-x-10 my-6 lg:my-12">
+        <div class="col-span-3 lg:col-span-1 grid lg:grid-rows-3">
+          <div>
+            <div class="w-32 mx-auto lg:w-24">
+              <img src="~/assets/images/Call_us.svg"/>
+            </div>
+          </div>
+          <div>
+            <p class="text-xl font-extrabold text-center text-logiclytextgrey mt-2">
+              Call us
+            </p>
+            <hr class="hidden lg:block w-full mx-auto border border-logiclyorange mt-6">
+          </div>
+          <div>
+            <p class="text-center text-logiclyorange mt-2 lg:-mt-2">
+              <a href="tel:0061393409000">
+                +61 3 9340 9000
+              </a>
+            </p>
+            <hr class="block lg:hidden w-full mx-auto border border-logiclyorange mt-12 lg:mt-6 mb-12">
           </div>
         </div>
-        <div class="grid grid-cols-1 my-16 lg:grid-cols-8">
-          <div class="grid grid-cols-1 lg:col-span-6 lg:col-start-2">
-            <div class="grid grid-cols-1 gap-5 lg:grid-cols-3">
-              <div class="grid col-span-1 grid-rows-4 p-5 bg-white md:grid-rows-5">
-                <div class="row-span-2 row-start-1">
-                  <div class="w-32 mx-auto lg:w-24">
-                    <img src="~/assets/images/Call_us.svg"/>
-                  </div>
-                </div>
-                <div class="grid grid-cols-1 col-span-1 row-span-1 row-start-3">
-                  <p class="text-xl font-extrabold text-center text-logiclytextgrey">
-                    Call us
-                  </p>
-                </div>
-                <div class="grid grid-cols-1 col-span-1 row-span-1 row-start-4">
-                  <p class="pt-2 font-normal text-center text-logiclyorange md:pt-0">
-                    <a href="tel:0061393409000">
-                      +61 3 9340 9000
-                    </a>
-                  </p>
-                  <hr class="w-full mx-auto border border-logiclyorange lg:order-first">
-                </div>
-              </div>
 
-              <div class="grid col-span-1 grid-rows-4 p-5 bg-white md:grid-rows-5">
-                <div class="row-span-2 row-start-1">
-                  <div class="w-32 mx-auto lg:w-24">
-                    <img src="~/assets/images/Email_us.svg"/>
-                  </div>
-                </div>
-                <div class="grid grid-cols-1 col-span-1 row-span-1 row-start-3">
-                  <p class="text-xl font-extrabold text-center text-logiclytextgrey">
-                    Email us
-                  </p>
-                </div>
-                <div class="grid grid-cols-1 col-span-1 row-span-1 row-start-4">
-                  <p class="pt-0 font-normal text-center text-logiclyorange">
-                    <a href="mailto:info@strategicdata.com.au">
-                      info@strategicdata.com.au
-                    </a>
-                  </p>
-                  <hr class="w-full mx-auto border border-logiclyorange lg:order-first">
-                </div>
-              </div>
-
-              <div class="grid col-span-1 grid-rows-4 p-5 bg-white md:grid-rows-5">
-                <div class="row-span-2 row-start-1">
-                  <div class="w-32 mx-auto lg:w-24">
-                    <img src="~/assets/images/Mail_us.svg"/>
-                  </div>
-                </div>
-                <div class="grid grid-cols-1 col-span-1 row-span-1 row-start-3">
-                  <p class="text-xl font-extrabold text-logiclytextgrey text-center">
-                    Mail us
-                  </p>
-                </div>
-                <div class="grid grid-cols-1 col-span-1 row-span-1 row-start-4">
-                  <div class="grid items-center justify-center grid-cols-1">
-                    <p class="w-full pt-1 font-light text-center text-logiclytextgrey">
-                      PO Box 1166, Carlton, <br>
-                      Victoria, Australia 3053
-                    </p>
-                    <hr class="w-full mx-auto border border-logiclyorange lg:order-first">
-                  </div>
-                </div>
-              </div>
+        <div class="col-span-3 lg:col-span-1 grid lg:grid-rows-3">
+          <div>
+            <div class="w-32 mx-auto lg:w-24">
+              <img src="~/assets/images/Email_us.svg"/>
             </div>
+          </div>
+          <div>
+            <p class="text-xl font-extrabold text-center text-logiclytextgrey mt-2">
+              Email us
+            </p>
+            <hr class="hidden lg:block w-full mx-auto border border-logiclyorange mt-6">
+          </div>
+          <div>
+            <p class="text-center text-logiclyorange mt-2 lg:-mt-2">
+              <a href="mailto:info@strategicdata.com.au">
+                info@strategicdata.com.au
+              </a>
+            </p>
+            <hr class="block lg:hidden w-full mx-auto border border-logiclyorange mt-12 lg:mt-6 mb-12">
+          </div>
+        </div>
+
+        <div class="col-span-3 lg:col-span-1 grid lg:grid-rows-3">
+          <div>
+            <div class="w-32 mx-auto lg:w-24">
+              <img src="~/assets/images/Mail_us.svg"/>
+            </div>
+          </div>
+          <div>
+            <p class="text-xl font-extrabold text-logiclytextgrey text-center mt-2">
+              Mail us
+            </p>
+            <hr class="hidden lg:block w-full mx-auto border border-logiclyorange mt-6">
+          </div>
+          <div>
+            <p class="font-light text-center text-logiclytextgrey mt-2 lg:-mt-2">
+              PO Box 1166, Carlton, <br>
+              Victoria, Australia 3053
+            </p>
+            <hr class="block lg:hidden w-full mx-auto border border-logiclyorange mt-12 lg:mt-6 mb-12">
           </div>
         </div>
       </div>
-    </div>
+    </page-section>
+
+
+
+
+
 
     <div class="w-full bg-logiclygreytwo">
       <div class="container mx-auto">
