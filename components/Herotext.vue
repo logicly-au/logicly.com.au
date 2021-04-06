06b7252d0ae81d8a3d498c4088c57fb0354a538c
Emmie Berg <emmieb@strategicdata.com.au>
2021-04-06 17:20:09 +1000

    Updating width of herotext component

--- components/Herotext.vue
+++ components/Herotext.vue
@@ -1,18 +1,15 @@
 i <template>
 <page-section :altrow="altrow">
-    <div class="flex justify-center ">
-      <div class="justify-center">
-        <img class="h-32" :src="iconurl" />
-      </div>
-    </div>
-    <div class="grid grid-cols-12">
-    <div class="col-span-12 xl:col-span-10 col-start-1 xl:col-start-2">
-      <div class="text-center text-logiclytextgrey">
-        <h1 class="text-2xl font-extrabold text-center pb-2 pt-4 xl:pt-8">{{ heading }}</h1>
-        <h2 class="font-light text-lg">{{ subheading }}</h2>
-      </div>
+  <div class="flex justify-center ">
+    <div class="justify-center">
+      <img class="h-32" :src="iconurl" />
     </div>
   </div>
+  <div class="mx-0 xl:px-6">
+    <div class="text-center text-logiclytextgrey">
+      <h1 class="text-2xl font-extrabold text-center pb-2 pt-4 xl:pt-8">{{ heading }}</h1>
+      <h2 class="font-light text-lg">{{ subheading }}</h2>
+    </div>
   </div>
 </page-section>
 </template>
