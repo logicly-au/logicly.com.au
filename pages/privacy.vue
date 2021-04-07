368c96cbddfbfa107b1be7ae8ca1e8db453ac90b
Emmie Berg <emmieb@strategicdata.com.au>
2021-04-07 10:15:11 +1000

    Styling 'Privacy' page and making it responsive'

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -1,99 +1,67 @@
 <template>
   <div>
+    <Hero
+      heading="YOUR PRIVACY"
+      subheading="Protecting your personal information"
+      background-url="Protecting_your_personal_information.jpg"
+      overlay
+    />
 
-    <div class="object-contain object-center w-full h-64 bg-cover"
-      :style="{ backgroundImage: `url(${backgroundUrl})` }">
-      <div class="container mx-auto">
-        <div class="grid h-64 grid-rows-3 grid-cols-15">
-          <div class="row-start-2 text-center text-white col-span-15">
-            <div>
-              <h1 class="text-lg font-bold">YOUR PRIVACY</h1>
-              <h2 class="text-xl font-bold">Protecting your personal information</h2>
-            </div>
-          </div>
-        </div>
+
+    <Herotext
+      iconurl="Data_privacy_principles.svg"
+      heading="Data privacy principles"
+      subheading="In general, you can visit this website without providing any personal information to us. If you choose to provide information voluntarily, we will use that information in accordance with our data privacy principles and in accordance with any specific notice given as part of the collection of that data."
+      altrow
+    />
+
+
+    <page-section>
+      <div class="text-logiclytextgrey px-0 lg:px-4 xl:px-6">
+        <p class="mb-4 text-lg">Strategic Data has always recognised the importance of maintaining the privacy of personal and sensitive information of our employees, clients, vendors and partners. The nature of our business requires us to collect and handle such information, and we have a responsibility to protect this information for as long as it is in our possession.</p>
+        <p class="mb-4 text-lg">At Strategic Data, respecting our employees, clients, vendors and partners is a part of our core values, along with being committed to providing services in an ethical manner.</p>
+        <p class="mb-8 text-lg">To support these obligations, Strategic Data has created a set of data privacy principles, which guide our efforts as they relate to privacy and the handling and protection of personal and sensitive information.</p>
+
+        <h1 class="text-xl font-extrabold mb-2">We respect your privacy by:</h1>
+        <ul class="text-base list-disc leading-7 mb-8 pl-5">
+          <li>Offering privacy notices that explain how and why we handle personal information.</li>
+          <li>Respecting your choices about our collection, use and sharing of information, where appropriate.</li>
+          <li>Collecting, using and retaining only personal information that is relevant and useful to our business interactions and the projects we undertake for our clients.</li>
+          <li>Using reasonable efforts to keep personal information accurate and up-to-date.</li>
+          <li>Limiting access to and disclosure of personal information.</li>
+          <li>Retaining only the personal information that is needed to fill our legal and business obligations.</li>
+          <li>Offering you the ability to view and update your personal information, where appropriate.</li>
+          <li>Providing you with an opportunity to ask questions and register complaints about privacy.</li>
+        </ul>
+
+        <h1 class="text-xl font-extrabold mb-2">We only collect and process your personal information for these purposes:</h1>
+        <ul class="text-base leading-7 list-disc mb-8 pl-5">
+          <li>to maintain our contractual or business relationship with you,</li>
+          <li>for employment-related services where applicable,</li>
+          <li>to tell you about the products and services we offer,</li>
+          <li>to contact and correspond with you, and</li>
+          <li>for the management and defence of legal claims and actions, compliance with court orders and other legal obligations and regulatory requirements, and as otherwise permitted by law.</li>
+        </ul>
+
+        <h1 class="text-xl font-extrabold mb-2">Website practices</h1>
+        <p class="mb-4 text-base"><span class="font-extrabold">Non-Identifiable Information</span> - Like many other Web sites, this Web site automatically collects certain non-identifiable information. Some of this is collected as an inherent part of the browsing process – e.g. your IP address, your ISP’s IP address, your browser, etc. For some other information, you have a choice about its provision – for example, cookie use, downloading / uploading file, etc. This non-identifiable information is used for Web sites and system administration purposes and to improve our online services. Your non-identifiable information may be disclosed to others and permanently archived for future use. This information is used to track Web site use and is not used to link to personal data.</p>
+        <p class="mb-4 text-base"><span class="font-extrabold">Cookies</span> - This Web site uses “cookies”, a technology that installs information on a Web site user’s computer to permit the Web site to recognise future visits using that computer. Cookies enhance the convenience and use of the Web site. For example, the information provided through cookies is used to recognise you as a previous user of the Web site, to track your activity at the Web site in order to respond to your needs, to offer personalised Web page content and information for your use, to automatically populate online forms with your personal information for your convenience, and to otherwise facilitate your Web site experience.</p>
+        <p class="mb-8 text-base">You may choose to decline cookies if your browser permits, but doing so may affect your use of this Web site and your ability to access or use certain Web site features.</p>
+
+        <h1 class="text-xl font-extrabold mb-2">Google Adwords Remarketing</h1>
+        <ul class="text-base list-disc leading-7 mb-8 pl-5">
+          <li>Strategic Data uses remarketing to advertise online.</li>
+          <li>Third-party vendors, including Google, show our ads on sites across the internet.</li>
+          <li>Third-party vendors, including Google, use cookies to serve ads based on your past visits to this website.</li>
+          <li>Visitors can opt out of Google’s use of cookies by visiting the Ads Preference Manager or visitors can opt out of a third-party vendor’s use of cookies by visiting Network Advertising Initiative opt-out page.</li>
+        </ul>
+
+        <h1 class="text-xl font-extrabold mb-2">Other matters</h1>
+        <p class="mb-4 text-base"><span class="font-extrabold">Security</span> - Strategic Data works to keep our servers, applications and databases secure and free from unauthorised access and use, by using physical, administrative and technological measure to protect the information we maintain. Nevertheless, you should be conscious of the type of data you choose to transmit and the means by which you do so. If you have any concerns about the appropriate transmission of your data, feel free to contact us.</p>
+        <p class="mb-4 text-base"><span class="font-extrabold">Other Websites</span> - This Web site may contain links to other Web sites. Please be conscious that, once you have clicked to leave our site, the privacy principles of other providers apply. You will need to contact them if you require information about their privacy principles.</p>
+        <p class="mb-4 text-base"><span class="font-extrabold">Privacy Notice Changes</span> - Strategic Data may change, supplement or amend this Notice as it relates to your future use of this Web site from time to time, for any reason, by posting a revised Notice on this Web site. This notice was last updated in November 2015.</p>
       </div>
-    </div>
-
-        <div class="w-full bg-logiclylightgrey">
-      <div class="container py-8 mx-auto">
-        <div class="grid h-64 grid-rows-3 grid-cols-15">
-          <div class="flex justify-center row-span-1 row-start-1 col-span-15">
-            <img class="h-32" src="~/assets/images/We-help-people-understand-all-the-possibilities.svg" />
-          </div>
-          <div class="row-span-1 row-start-3 text-center col-span-15">
-            <h2 class="text-xl font-bold">
-              Data privacy principles
-            </h2>
-            <p>
-              In general, you can visit this website without providing any personal information to us. If you choose to provide information voluntarily, we will use that information in accordance with our data privacy principles and in accordance with any specific notice given as part of the collection of that data.
-            </p>
-          </div>
-
-        </div>
-      </div>
-    </div>
-
-
-
-
-
-
-        <div class="w-full">
-      <div class="container py-8 mx-auto">
-        <div class="grid grid-cols-15">
-
-<div class="col-span-15">
-Strategic Data has always recognised the importance of maintaining the privacy of personal and sensitive information of our employees, clients, vendors and partners. The nature of our business requires us to collect and handle such information, and we have a responsibility to protect this information for as long as it is in our possession. 
-At Strategic Data, respecting our employees, clients, vendors and partners is a part of our core values, along with being committed to providing services in an ethical manner.
-To support these obligations, Strategic Data has created a set of data privacy principles, which guide our efforts as they relate to privacy and the handling and protection of personal and sensitive information.
-
-We respect your privacy by:
-• Offering privacy notices that explain how and why we handle personal information.
-•	Respecting your choices about our collection, use and sharing of information, where appropriate.
-•	Collecting, using and retaining only personal information that is relevant and useful to our 				business interactions and the projects we undertake for our clients.
-•	Using reasonable efforts to keep personal information accurate and up-to-date.
-•	Limiting access to and disclosure of personal information.
-•	Retaining only the personal information that is needed to fill our legal and business obligations.
-•	Offering you the ability to view and update your personal information, where appropriate.
-	Providing you with an opportunity to ask questions and register complaints about privacy.
-
-We only collect and process your personal information for these purposes:
-• to maintain our contractual or business relationship with you,
-• for employment-related services where applicable,
-•	to tell you about the products and services we offer,
-•	to contact and correspond with you, and
-•	for the management and defence of legal claims and actions, compliance with court orders and 		other legal obligations and regulatory requirements, and as otherwise permitted by law.
-
-Website practices
-Non-Identifiable Information - Like many other Web sites, this Web site automatically collects certain non-identifiable information. Some of this is collected as an inherent part of the browsing process – e.g. your IP address, your ISP’s IP address, your browser, etc. For some other information, you have a choice about its provision – for example, cookie use, downloading / uploading file, etc. This non-identifiable information is used for Web sites and system administration purposes and to improve our online services. Your non-identifiable information may be disclosed to others and permanently archived for future use. This information is used to track Web site use and is not used to link to personal data.
-Cookies - This Web site uses “cookies”, a technology that installs information on a Web site user’s computer to permit the Web site to recognise future visits using that computer. Cookies enhance the convenience and use of the Web site. For example, the information provided through cookies is used to recognise you as a previous user of the Web site, to track your activity at the Web site in order to respond to your needs, to offer personalised Web page content and information for your use, to automatically populate online forms with your personal information for your convenience, and to otherwise facilitate your Web site experience.
-You may choose to decline cookies if your browser permits, but doing so may affect your use of this Web site and your ability to access or use certain Web site features.
-
-Google Adwords Remarketing
-•	Strategic Data uses remarketing to advertise online.
-•	Third-party vendors, including Google, show our ads on sites across the internet.
-•	Third-party vendors, including Google, use cookies to serve ads based on your past visits to 	this website.
-•	Visitors can opt out of Google’s use of cookies by visiting the Ads Preference Manager or visitors 		can opt out of a third-party vendor’s use of cookies by visiting Network Advertising Initiative 			opt-out page.
-
-Other matters
-Security - Strategic Data works to keep our servers, applications and databases secure and free from unauthorised access and use, by using physical, administrative and technological measure to protect the information we maintain. Nevertheless, you should be conscious of the type of data you choose to transmit and the means by which you do so. If you have any concerns about the appropriate transmission of your data, feel free to contact us.
-Other Websites - This Web site may contain links to other Web sites. Please be conscious that, once you have clicked to leave our site, the privacy principles of other providers apply. You will need to contact them if you require information about their privacy principles.
-Privacy Notice Changes - Strategic Data may change, supplement or amend this Notice as it relates to your future use of this Web site from time to time, for any reason, by posting a revised Notice on this Web site. This notice was last updated in November 2015.
-
-</div>
-</div>
-</div>
-
-
-
-
-</div>
-
-
-
-
-
+    </page-section>
 
 
   </div>
