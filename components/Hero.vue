d8045c1c81dddc7ac3533d10b21df0259e68aea4
Emmie Berg <emmieb@strategicdata.com.au>
2021-04-29 11:08:05 +1000

    Updating font size and font weight Hero component

--- components/Hero.vue
+++ components/Hero.vue
@@ -5,12 +5,10 @@
     width="100%"
     :style="{ backgroundSize: 'cover', backgroundImage: `url(${backgroundUrl})` }"
   >
-            <page-section :altrow="altrow" :overlay="overlay">
-
-              <h1 class="text-lg font-medium text-center text-white">{{ heading }}</h1>
-              <h2 class="text-2xl font-semibold text-center text-white">{{ subheading }}</h2>
-
-            </page-section>
+    <page-section :altrow="altrow" :overlay="overlay">
+      <h1 class="text-xs md:text-sm xl:text-base font-medium text-center text-white">{{ heading }}</h1>
+      <h2 class="text-base sm:text-lg md:text-xl xl:text-2xl font-semibold text-center text-white">{{ subheading }}</h2>
+    </page-section>
 
   </AspectRatio>
 </template>
@@ -32,16 +30,16 @@ export default {
 </script>
 
 <style scoped>
-.h-128 {
-height:32rem;
+  .h-128 {
+  height:32rem;
 }
 
 .bg-overlay {
-    background-color: rgba(248, 247, 216, 0.7);
-    position: relative;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
+  background-color: rgba(248, 247, 216, 0.7);
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
