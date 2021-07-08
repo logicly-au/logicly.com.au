139dafc91ac8194c3999afbd43c2070faff4871c
ash <ash@strategicdata.com.au>
2021-07-08 11:59:13 +1000

    Add temporary rebranding notification component
    This shows itself once then goes away forever!

---
+++ components/RebrandNotification.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="show">
+    We rebrand
+    <button @click="show = false">x</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RebrandNotification",
+
+  data() {
+    return { show: true };
+  },
+
+  created() {
+    const storageKey = 'stratdatrebrandseen';
+
+    if (localStorage.getItem(storageKey)) {
+      this.show = false;
+    } else {
+      localStorage.setItem(storageKey, true);
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
