ff04ffeef8eef07f21dfb52289db3345701a3f6c
Huw M <huwm@strategicdata.com.au>
2021-07-08 15:49:59 +1000

    Styled rebrand notification

--- components/RebrandNotification.vue
+++ components/RebrandNotification.vue
@@ -1,7 +1,10 @@
 <template>
-  <div v-if="show">
-    We rebrand
-    <button @click="show = false">x</button>
+  <div v-if="show" class="flex">
+    <!-- <h1>Why is Strategic Data now called Logicly? We do have <a href="/linkhere">a good reason</a></h1> -->
+    <div class="flex-auto">
+      <h1 class="py-3 text-center">Strategic Data is now called Logicly</h1>
+    </div>
+    <button class="flex-initial pr-3 text-xl text-logiclyorange" @click="show = false">&#10005;</button>
   </div>
 </template>
 
