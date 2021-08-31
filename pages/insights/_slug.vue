9d26f6458e60b27b6b9c193620b27b3301364acc
Emmie Berg <emmieb@strategicdata.com.au>
2021-08-31 11:34:17 +1000

    Fixes to 'Insights' page, adding styling to blog posts and fixing links

--- pages/insights/_slug.vue
+++ pages/insights/_slug.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="article">
 
-    <!-- Active blog post -->
-    <!-- TODO Move this to a separate page for active blog post -->
+    <Hero
+      heading="INSIGHTS"
+      subheading="In depth articles, discussions and thoughts on innovative digital systems, projects and topics of interest from our team."
+      background-url="/Logicly_website_photos_header-14.jpg"
+      black
+      overlay
+    />
+
+    <page-section altrow>
+      <ul>
+        <li>
+          <div class="grid grid-cols-12">
+            <div class="col-span-12 lg:col-span-3 text-logiclytextgrey">
+              <h2 class="pb-2 font-semibold">{{ article.category }}</h2>
+            </div>
+            <div class="col-span-12 lg:col-span-9 lg:col-start-4 lg:pl-6 blogheading">
+              <h1 class="pb-5 text-xl font-semibold leading-tight text-logiclyorange">{{ article.title }}</h1>
+              <span class="text-lg font-light text-logiclytextgrey" v-html="`${article.description}`"></span>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </page-section>
+
     <page-section>
       <div class="text-logiclytextgrey article-page">
         <nuxt-content :document="article" />
@@ -45,6 +67,46 @@
                     </div>
                     <div>
                       <span class="font-normal text-base lg:text-sm text-logiclytheme4 hover:underline">
+                        <NuxtLink :to="`${article.link}`">Read more</NuxtLink>
+                      </span>
+                    </div>
+                  </div>
+                </li>
+              </div>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <!-- TODO: Make this section show articles in the same category/topic -->
+      <div class="mt-6 border-t-2 border-logiclyorange lg:mt-16">
+        <div class="grid grid-cols-12 mt-10 lg:mt-12">
+          <div class="col-span-12 lg:col-span-3">
+            <p class="pb-8 text-lg font-semibold lg:text-base text-logiclytextgrey lg:pb-0">
+              Related articles
+            </p>
+          </div>
+          <div class="col-span-12 lg:col-span-9 lg:col-start-4 lg:pl-10">
+            <ul>
+              <div class="grid gap-5 lg:grid-cols-2 grid-rows-auto">
+                <li @click="setActiveArticle(index)" v-for="(article, index) in articles" :class="{ 'articles-active': isActiveArticle(index) }">
+                  <div class="">
+                    <div class="pb-5">
+                       <img class="w-full" :src="article.img" :alt="article.alt" />
+                    </div>
+                    <div>
+                      <span class="text-lg font-semibold lg:text-base text-logiclyorange">{{ article.title }}</span>
+                    </div>
+                    <div class="pb-2">
+                      <span class="text-sm font-medium">{{ article.author }}</span>
+                      <span class="text-logiclyorange">|</span>
+                      <span class="text-sm font-normal">{{ article.date }}</span>
+                    </div>
+                    <div class="pb-2">
+                      <span class="text-base font-light lg:text-sm" v-html="`${article.description}`"></span>
+                    </div>
+                    <div>
+                      <span class="text-base font-normal lg:text-sm text-logiclyorange hover:underline">
                         <NuxtLink :to="`${article.link}`">Read more</NuxtLink>
                       </span>
                     </div>
