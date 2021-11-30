c0d3cd467e4930b2704021f7fcb5faccca3880af
huwmillar <huw.millar@strategicdata.com.au>
2021-11-30 16:31:03 +1100

    Added mobile styling and tweaked copy

--- pages/strategicdata.vue
+++ pages/strategicdata.vue
@@ -2,18 +2,18 @@
 
     <div class="flex flex-col h-full">
     
-        <div class="flex content-center justify-center customflex">
-            <div class="img-container">
+        <div class="flex content-center justify-center flex-initial md:customflexmd">
+            <div class="m-5 img-container">
                 <img src="~/assets/images/logicly_splash_animation_800600.gif" class="object-contain w-full h-full" />
             </div>
         </div>   
-        <div class="flex content-center customflex bg-logiclylightgrey">
-            <div class="flex justify-center flex-auto align-middle text-logiclytextgrey">
+        <div class="flex content-center flex-auto md:customflexmd bg-logiclylightgrey">
+            <div class="flex justify-center flex-auto md:align-middle text-logiclytextgrey">
             
-                <div class="self-center">
-                    <h1 class="pb-4 text-3xl md:text-5xl">Strategic Data is now called Logicly.</h1><br />
-                    <h2 class="text-sm md:text-2xl">Why is Strategic Data now called Logicly? We have  <NuxtLink to="/whylogicly"><span class="font-semibold text-logiclyorange">a good reason</span></NuxtLink>.</h2><br />
-                    <h2 class="text-sm md:text-2xl">Trust us and just want to get on with things? Proceed straight to our new website <NuxtLink to="/whylogicly"><span class="font-semibold text-logiclyorange">here</span><br /> or wait to be automatically redirected in {{ timerCount }}...</NuxtLink>
+                <div class="self-center m-5">
+                    <h1 class="pb-4 text-3xl md:text-6xl">Strategic Data is now called Logicly.</h1><br />
+                    <h2 class="text-2xl md:text-4xl">Why is Strategic Data now called Logicly? We have  <NuxtLink to="/whylogicly"><span class="font-semibold text-logiclyorange">a good reason</span></NuxtLink>.</h2><br />
+                    <h2 class="text-2xl md:text-4xl">Just want to get on with things? Proceed straight to our new website <NuxtLink to="/whylogicly"><span class="font-semibold text-logiclyorange">here</span><br /> or wait to be automatically redirected in {{ timerCount }}...</NuxtLink>
                     </span></h2>
                 </div>
                 
@@ -29,7 +29,7 @@
 export default {
     layout: 'splashpage',
     created() {
-        setTimeout( () => this.$router.push({ path: '/'}), 15000);
+        setTimeout( () => this.$router.push({ path: '/'}), 10000);
     },
     head() {
     return {
@@ -38,7 +38,7 @@ export default {
     },
     data() {
         return {
-            timerCount: 15
+            timerCount: 10
         }
     },
     watch: {
@@ -49,7 +49,7 @@ export default {
                 if (value > 0) {
                     setTimeout(() => {
                         this.timerCount--;
-                    }, 1500);
+                    }, 1000);
                 }
 
             },
@@ -67,7 +67,7 @@ div#__nuxt, div#__layout {
     height:100vh;
     }
 
-.customflex {
+.customflexmd {
     flex: 1 1 50%;
 }
 
