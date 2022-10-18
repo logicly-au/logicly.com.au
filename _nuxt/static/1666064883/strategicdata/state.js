a397f74ddb204d1c2af70e671d46d6814b1b117f
huwmillar <huwmillar@users.noreply.github.com>
2022-10-18 03:48:16 +0000

    deploy: 7517e2c8ed3b04f40f6c608d02bb1d14c5ccf94d

---
+++ _nuxt/static/1666064883/strategicdata/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1666064883",layout:"splashpage",error:a,serverRendered:true,routePath:"\u002Fstrategicdata",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f6a8adc8"}}}}(null));
