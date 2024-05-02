8ff081c1979b20f6c19ebecb09bad71001b4c0a9
Emmieb <emmieb@strategicdata.com.au>
2024-05-02 13:25:12 +1000

    Adding new 'Scholarship' page

---
+++ pages/scholarship.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <page-section altrow>
+      <div class="px-0 xl:px-6">
+        <div class="text-center">
+          <h1 class="text-3xl font-semibold text-logiclytheme2 xl:text-4xl">Perl Training Scholarship</h1>
+          <h2 class="text-2xl font-light">Sponsored by Logicly Pty Ltd</h2>
+        </div>
+      </div>
+    </page-section>
+
+    <page-section>
+      <div class="px-0 xl:px-6 scholarship-link">
+        <div class="text-logiclytextgrey">
+          <h3 class="pb-2 text-lg">Logicly is offering up to eight individual scholarships for computer science graduates to complete a five-day <i>Introduction to Perl for Programmers</i> course, run by Paul Fenwick and valued at over $2,500 per person.</h3>
+          <h3 class="pb-6 text-lg">The scholarships are a fantastic opportunity for the successful applicants to receive a world-class introduction to the popular Perl language and to obtain the background and technical skills necessary to begin developing code in a professional setting.</h3>
+
+          <h4 class="pb-1 text-lg font-bold">Course participants will also be considered for a Graduate Programmer position at Logicly</h4>
+          <p class="pb-4 font-light">Applicants will ideally be recent computer science graduates or computer science students who are soon to complete their degree.</p>
+
+          <h4 class="pb-1 text-lg font-bold">What is Introduction to Perl for Programmers?</h4>
+          <p class="pb-2 font-light">Introduction to Perl for Programmers is an extended Perl workshop for those with a solid programming background but no specific or formal training in Perl. It familiarises students with the basic structure and design of the language and explores many of the features that have made Perl such a widely used and widely useful development platform.</p>
+          <p class="pb-2 font-light">The course will be run by Paul Fenwick, one of the world’s foremost Perl trainers.</p>
+          <p class="pb-4 font-light">Scholarship recipients will attend the five-day course to be run at Logicly’s office in Fitzroy from Monday 1st July to Friday 5th July, 2024.</p>
+
+          <h4 class="pb-1 text-lg font-bold">Who is Logicly?</h4>
+          <p class="pb-2 font-light">Logicly is a Melbourne based data-focused software development business providing services to Government, universities and corporates. With a heavy emphasis on Perl and open-source software, we specialise in user-focused on-line data collection and management projects. We work closely with our clients to develop user-friendly, secure, and considered digital information management solutions in areas such as mental health, education and transport.</p>
+          <p class="pb-2 font-light">Logicly is a collaborative, innovative organisation that values learning and growth.</p>
+          <p class="pb-4 font-light">Incorporated in 1996, we also trade under the name WebSurvey and were previously named “Strategic Data”. For more information about us, see <NuxtLink to="/" class="hover:underline" exact>https://www.logicly.com.au</NuxtLink> and <a href="http://www.websurvey.com.au">http://www.websurvey.com.au</a></p>
+
+          <h4 class="pb-1 text-lg font-bold">Who is Paul Fenwick?</h4>
+          <p class="pb-4 font-light">As well as having previously been central to Australia’s dedicated Perl training organisation (Perl Training Australia), Paul Fenwick is an internationally acclaimed public speaker and teacher. He is recipient of both the 2010 White Camel award and the 2013 O'Reilly Open-Source award, both for outstanding contributions to the Perl Community. Paul is technical editor of the Perl Best Practices book, author of Perl's autodie module, and has keynoted at technical conferences worldwide. Paul is an engaging and entertaining teacher.  In his spare time Paul is fond of weaving baskets with the help of his cat, Queen Felicia.</p>
+
+          <h4 class="pb-1 text-lg font-bold">History of Perl Scholarships</h4>
+          <p class="pb-4 font-light">Logicly has periodically run Perl Scholarships as a development opportunity for young developers, as well as encouraging Perl-awareness. We have also recruited a number of developers through this process.</p>
+
+          <h4 class="pb-1 text-lg font-bold">Why apply?</h4>
+          <p class="pb-4 font-light">If you’re about to graduate or have recently done so, have an interest in Perl or would like to know more, then this scholarship is for you. It’s a great opportunity to undertake a highly regarded, practically focused course free of charge. It’s also a fantastic chance to meet industry contacts, improve your resume and be considered for a Graduate Programmer position at Logicly.</p>
+
+          <h4 class="pb-1 text-lg font-bold">Selection Criteria</h4>
+          <p class="pb-2 font-light">Up to eight scholarships covering the complete cost of Introduction to Perl for Programmers are available. Participants will be selected on the basis of:</p>
+          <ul class="pb-2 pl-4 font-light list-disc">
+            <li>Demonstrated interest in Perl and/or open-source software</li>
+            <li>Academic performance</li>
+            <li>Interest in, and capacity for potential employment with Logicly (Fitzroy – Melbourne)</li>
+          </ul>
+
+          <p class="pb-2 font-light">To apply, please send a brief letter detailing:</p>
+          <ul class="pb-4 pl-4 font-light list-disc">
+            <li>Why you would like to learn or improve your knowledge of Perl</li>
+            <li>Any experience you have indicating your interest in Perl and/or open-source software</li>
+            <li>Your academic results</li>
+            <li>Your interest in employment with Logicly</li>
+          </ul>
+
+          <h4 class="pb-1 text-lg font-bold">Please send your application by email to:</h4>
+          <p class="pb-4 pl-6">Andrew Gordon <br>scholarship@logicly.com.au</p>
+          <p class="pb-2 font-light">Applications should be lodged as early as possible – selection process and offering of places will commence on 25th May.</p>
+          <p class="pb-2 font-light">For all questions or enquiries please contact Andrew Gordon on <a href="tel:+61393409000">03 9340 9000.</a></p>
+
+        </div>
+      </div>
+    </page-section>
+
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Scholarship · Logicly"
+    };
+  },
+};
+</script>
+
+<style>
+.scholarship-link a {
+  color:#E6332A!important;
+  font-weight: 500;
+  text-decoration: none;
+}
+
+.scholarship-link a:hover {
+  text-decoration: underline;
+}
+
+.scholarship-link a:visited {
+  color:#A83914!important;
+}
+</style>
