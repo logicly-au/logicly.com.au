01da94941cf3b4c19a9e37ba235d0eb31a923657
Emmieb <emmieb@strategicdata.com.au>
2025-10-21 16:26:44 +1100

    Touch target and semantics fix for button and CTA components

--- components/CTA.vue
+++ components/CTA.vue
@@ -3,31 +3,28 @@
   <page-section>
     <div class="content-center object-contain object-center w-full text-logiclytextgrey">
       <h2 class="text-2xl font-semibold leading-snug text-center text-logiclytextgrey">{{ text }}</h2>
-      <div class="grid justify-center pt-4 align-center">
-        <button class="block w-40 h-12 my-5 font-medium text-center text-white bg-logiclyorange hover:bg-logiclyhover">
-          <NuxtLink :to="buttonLink">{{ buttonText }}</NuxtLink>
-        </button>
+      <div class="grid justify-center p-4 align-center">
+        <NuxtLink :to="buttonLink" class="flex items-center justify-center w-40 h-12 my-5 lowercase font-medium text-center text-white bg-logiclyorange hover:bg-logiclyhover">
+          {{ buttonText }}
+        </NuxtLink>
       </div>
     </div>
   </page-section>
 </template>
 
 <script>
-  export default {
-    name: "CTA",
+export default {
+  name: "CTA",
 
     // This is how you define all the attributes passed to the component
     // backgroundUrl becomes background-url when used as html attribute
-    props: {
-      text: String,
-      buttonLink: String,
-      buttonText: String
-    },
-  };
+  props: {
+    text: String,
+    buttonLink: String,
+    buttonText: String
+  },
+};
 </script>
 
 <style scoped>
-button > a {
-  text-transform: lowercase!important;
-}
 </style>
